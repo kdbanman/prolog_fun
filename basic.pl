f3c4243bbf4a582d05a56fd2bd8fb686fc624d25
kdbanman <kirby.banman@gmail.com>
2016-03-08 11:56:40 -0700

    write a broken uniqueness query and a working member query.

--- basic.pl
+++ basic.pl
@@ -1,9 +1,38 @@
-last([X], X).
+xlast([X], X).
 
-last([H|T], X) :- last(T, X).
+xlast([_|T], X) :- xlast(T, X).
+
 
 xreverse([X], [X]).
 
-xreverse([X, Y], [Y, X]).
+xreverse([H|T], R) :- 
+    xreverse(T, Rt), 
+    append(Rt, [H], R).
 
-%xreverse(append(L, [X], [
+
+xunique([X], [X]).
+
+xunique([H|T], [H|U]) :-
+    xunique(T, U).
+
+/*
+xunique(L, U) :-
+    xunique_accum(L, U, []).
+
+xunique_accum([X], [X], _).
+
+xunique_accum([H|T], [H|U], A) :-
+    xunique_accum(T, U, [H|A]).
+*/
+
+/*
+xunique_accum([H1|T], [H2|U], A) :-
+    H1 \= H2,
+    xmember(H1, A),
+    xunique_accum(T, [H2|U], A).
+*/
+
+xmember(X, [X|_]).
+
+xmember(X, [_|T]) :- xmember(X, T).
+    
