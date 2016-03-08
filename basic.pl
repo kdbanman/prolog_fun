52098907254ccb0df95bc32a21c85f366933c23f
Kirby Banman <kdbanman@ualberta.ca>
2016-03-08 16:33:54 -0700

    get a more complex but still broken version of uniqueness running.

--- basic.pl
+++ basic.pl
@@ -13,16 +13,39 @@ xreverse([H|T], R) :-
 % Base case, TODO one element or empty.
 xunique([], []).
 
-% Eliminate from U if not in T
+% If the head of the list is repeated in the tail, do not alter the 
+% unique member list.
 xunique([H|T], U) :-
-    select(H, U, Up),
     xmember(H, T),
     xunique(T, U).
 
+% If the head of the list is not repeated in the tail
 xunique([H|T], U) :-
-    select(H, U, Up),
     xnotmember(H, T),
+    select(H, U, Up),
     xunique(T, Up).
+
+
+xu([], [], _).
+
+% If the head is not repeated in the tail, drop it from the unique members.
+xu([H|T], [H|U], A) :-
+    xnotmember(H, T),
+    append(A, [H], Ap),
+    xu(T, U, Ap).
+
+% If the head is repeated in the tail, don't drop it from the unique members.
+xu([H|T], [H|U], A) :-
+    xmember(H, T),
+    xu(T, [H|U], A).
+
+xu([H|T], U, A) :-
+    xmember(H, A),
+    xu(T, U, A).
+
+xu([H|T], U, A) :-
+    xmember(H, U),
+    xu(T, U, A).
 
 
 xnotmember(X, []).
